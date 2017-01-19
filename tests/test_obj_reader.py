fe456ce9a677d8122e9b1b55c4a48985b35c3f75
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2017-01-19 14:07:53 +0100

    Added new file that helps test for correct number of meshes detected.

--- tests/test_obj_reader.py
+++ tests/test_obj_reader.py
@@ -12,7 +12,13 @@ import pytest
 from wavefront_reader import read_objfile
 
 filepath = path.join(path.split(__file__)[0], '..', 'examples')
-filenames = ['untitled.obj', 'untitled_with_normals.obj', 'untitled_with_normals_and_texcoords.obj']
+
+filenames = ['untitled.obj',
+             'untitled_with_normals.obj',
+             'untitled_with_normals_and_texcoords.obj',
+             'two_complete_meshes.obj'
+             ]
+
 fnames = [path.join(filepath, name) for name in filenames]
 
 @pytest.mark.parametrize("objfile", fnames)
@@ -20,7 +26,7 @@ def test_files_exist(objfile):
         assert path.exists(objfile)
 
 
-@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [True, True, True])))
+@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [True, True, True, True])))
 def test_has_vertices(objfile, expected):
     with open(objfile) as f:
         geoms = read_objfile(f)
@@ -28,7 +34,7 @@ def test_has_vertices(objfile, expected):
     assert (len(cube['v']) > 0) == expected
 
 
-@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [False, True, True])))
+@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [False, True, True, True])))
 def test_has_normals(objfile, expected):
     with open(objfile) as f:
         geoms = read_objfile(f)
@@ -36,7 +42,7 @@ def test_has_normals(objfile, expected):
     assert (len(cube['vn']) > 0) == expected
 
 
-@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [False, False, True])))
+@pytest.mark.parametrize("objfile,expected", list(zip(fnames, [False, False, True, True])))
 def test_has_texcoords(objfile, expected):
     with open(objfile) as f:
         geoms = read_objfile(f)
@@ -48,9 +54,17 @@ def test_has_texcoords(objfile, expected):
 def test_has_texcoords(objfile):
     with open(objfile) as f:
         geoms = read_objfile(f)
-    cube = geoms['Cube']
-    vertrows = len(cube['v'])
-    for coord in ['vn', 'vt']:
-        if len(cube[coord]):
-            assert len(cube[coord]) == vertrows
+    for geom in geoms.values():
+        vertrows = len(geom['v'])
+        for coord in ['vn', 'vt']:
+            if len(geom[coord]):
+                assert len(geom[coord]) == vertrows
+
+
+@pytest.mark.parametrize("objfile, count", list(zip(fnames, [1, 1, 1, 2])))
+def test_has_correct_number_of_meshes(objfile, count):
+    with open(objfile) as f:
+        geoms = read_objfile(f)
+    assert len(geoms) == count
+
 
