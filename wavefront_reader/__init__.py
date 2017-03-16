e5368bfe1290e331571e224bf89f24fda8432782
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2017-03-16 11:16:34 +0100

    edited module name

--- wavefront_reader/__init__.py
+++ wavefront_reader/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Nicholas A. Del Grosso"""
 __email__ = 'delgrosso@bio.lmu.de'
 __version__ = '0.1.0'
 
-from .wavefront_reader import read_objfile, parse_mixed_delim_str, read_mtlfile, read_wavefront
+from .reading import read_objfile, parse_mixed_delim_str, read_mtlfile, read_wavefront
