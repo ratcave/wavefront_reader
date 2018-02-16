2714ab214f406a6d9431703d14eb532064753ba4
Daniel Coelho <daniel.coelho@navegatium.com>
2018-02-16 16:12:02 -0200

    Add example of an obj file with bad syntax and no material
    The lib can handle it now too, as well as all the other examples and tests. Also adds the filename to the relevant tests (obviously material-related tests should not include it)

--- tests/test_wavefront_reader.py
+++ tests/test_wavefront_reader.py
@@ -9,19 +9,20 @@ filepath = path.join(path.split(__file__)[0], '..', 'examples')
 filenames = ['untitled.obj',
              'untitled_with_normals.obj',
              'untitled_with_normals_and_texcoords.obj',
-             'two_complete_meshes.obj'
+             'two_complete_meshes.obj',
+             'bad_file.obj'
              ]
 
 fnames = [path.join(filepath, name) for name in filenames]
 
 
-@pytest.mark.parametrize("fn, count", zip(fnames, [1, 1, 1, 2]))
+@pytest.mark.parametrize("fn, count", zip(fnames, [1, 1, 1, 2, 1]))
 def test_all_materials_extracted(fn, count):
     geoms = read_wavefront(fn)
     assert len(geoms) == count
 
-
-@pytest.mark.parametrize("fn", fnames)
+# Ignore 'bad_file.obj', it purposefully has no material
+@pytest.mark.parametrize("fn", fnames[0:-1])
 def test_geoms_have_material_dict(fn):
     geoms = read_wavefront(fn)
     for geom in geoms.values():
